b5ebaae2f2610b868b1c4a6388c75bd73add80b8
Jaya chithra P <113277580+Jayachithra0205@users.noreply.github.com>
2023-08-05 17:24:56 +0530

    Add files via upload

---
+++ Level1_Task2/index.js
@@ -0,0 +1,26 @@
+
+function changeColor1(){
+    const bg = document.querySelectorAll("BackgroundBox");
+    bg.style.background = "linear-gradient(to bottom right,#f33b69, #FED9B7)";
+    bg.style.boxShadow = "inset -5px -5px 8px rgba(244, 10, 57, 0.432),inset 5px 5px 8px #f6f6f669";
+    const bg2 = document.querySelectorAll(".BackgroundBox2");
+    bg2.style.background = "linear-gradient(to bottom left,#f00e47, #f7b77c)";
+    bg2.style.boxShadow = "inset -5px -5px 5px rgba(247, 4, 53, 0.432),inset 5px 5px 8px #f6f6f656";
+}
+function changeColor2(){
+    const bg = document.querySelectorAll("BackgroundBox");
+    bg.style.background = "linear-gradient(to bottom right,#00b972,#fed9b7)";
+    bg.style.boxShadow = "inset -5px -5px 8px #015535,inset 5px 5px 8px #f6f6f694";
+    const bg2 = document.querySelectorAll(".BackgroundBox2");
+    bg2.style.background = "background:linear-gradient(to bottom left,#01301e,#70cfab);";
+    bg2.style.boxShadow = "inset -5px -5px 8px #015535,inset 5px 5px 8px #f6f6f694";
+}
+function changeColor3(){
+    const bg = document.querySelectorAll("BackgroundBox");
+    bg.style.background = "linear-gradient(to bottom right,#00b972,#fed9b7)";
+    bg.style.boxShadow = "inset -5px -5px 8px #015535,inset 5px 5px 8px #f6f6f694";
+    const bg2 = document.querySelectorAll(".BackgroundBox2");
+    bg2.style.background = "background:linear-gradient(to bottom left,#01301e,#70cfab);";
+    bg2.style.boxShadow = "inset -5px -5px 8px #015535,inset 5px 5px 8px #f6f6f694";
+} 
+
